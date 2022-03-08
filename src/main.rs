4e03fb8dd370f33f7c46e334abffcf61dea23fe2
Kirill Priadko <pk1d3v@gmail.com>
2022-03-08 11:50:44 +0300

    Day 1. Part two

--- src/main.rs
+++ src/main.rs
@@ -21,16 +21,32 @@ fn day01() -> Result<(), Box<dyn Error>> {
         .map(|l| l.trim().parse().unwrap())
         .collect();
 
-    let res: u64 = modules
+    let ans1: u32 = modules
         .iter()
-        .fold(0u64, |sum, val| sum + calc_fuel_for_module(*val) as u64);
+        .fold(0u32, |sum, val| sum + calc_fuel(*val));
 
-    println!("answer: {}", res);
+    let ans2: u32 = modules
+        .iter()
+        .fold(0u32, |sum, val| sum + calc_fuel_total(*val));
+
+    println!("answer 1: {}", ans1);
+    println!("answer 2: {}", ans2);
     Ok(())
 }
 
-fn calc_fuel_for_module(mass: u32) -> u32 {
-    return mass / 3 - 2;
+fn calc_fuel(mass: u32) -> u32 {
+    return (mass / 3).saturating_sub(2);
+}
+
+fn calc_fuel_total(module_mass: u32) -> u32 {
+    let mut ans = 0;
+    let mut mass = module_mass;
+    while mass > 0 {
+        mass = calc_fuel(mass);
+        ans += mass;
+    }
+
+    return ans + mass;
 }
 
 #[cfg(test)]
@@ -39,9 +55,16 @@ mod tests {
 
     #[test]
     fn test_calc_fuel() {
-        assert_eq!(calc_fuel_for_module(12), 2);
-        assert_eq!(calc_fuel_for_module(14), 2);
-        assert_eq!(calc_fuel_for_module(1969), 654);
-        assert_eq!(calc_fuel_for_module(100756), 33583);
+        assert_eq!(calc_fuel(12), 2);
+        assert_eq!(calc_fuel(14), 2);
+        assert_eq!(calc_fuel(1969), 654);
+        assert_eq!(calc_fuel(100756), 33583);
+    }
+
+    #[test]
+    fn test_calc_total() {
+        assert_eq!(calc_fuel_total(14), 2);
+        assert_eq!(calc_fuel_total(1969), 966);
+        assert_eq!(calc_fuel_total(100756), 50346);
     }
 }
